4097269b6bc0615c02ebb98a79da52630a19976d
Lucas <lucasdebruchard@gmail.com>
2022-06-25 12:46:37 -0300

    Desafio-TypeScript

---
+++ Desafio2/desafio2.ts
@@ -0,0 +1,30 @@
+interface pessoa {
+    nome: string,
+    idade: number,
+    profissao: string
+}
+
+let pessoa1: pessoa = {
+    nome: 'maria',
+    idade: 29,
+    profissao: "atriz"
+}
+
+
+let pessoa2: pessoa = {
+nome: "roberto",
+idade: 19,
+profissao: "Padeiro",
+}
+
+let pessoa3: pessoa = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+
+let pessoa4: pessoa = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
---
+++ Desafio2/desafio3.js
@@ -0,0 +1,31 @@
+// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+var botaoAtualizar = document.getElementById('atualizar-saldo');
+var botaoLimpar = document.getElementById('limpar-saldo');
+var soma = document.getElementById('soma');
+var campoSaldo = document.getElementById('campo-saldo');
+var saldoTotal = 0;
+limparSaldo();
+function somarAoSaldo(soma) {
+    if (campoSaldo) {
+        saldoTotal += soma;
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+function limparCampoSoma() {
+    soma.value = "";
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', function () {
+        somarAoSaldo(Number(soma.value));
+    });
+}
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
